4b101d53ce568864e766beb9bb145d26a5e96067
JDevede <95028621+JDevede@users.noreply.github.com>
2023-12-19 18:29:36 +0100

    Update reptisell.py

--- reptisell.py
+++ reptisell.py
@@ -1,64 +1,23 @@
-import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import os
-PORT = int(os.environ.get('PORT', '8443'))
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+# Define a function to handle the /start command
+def start(update: Update, context: CallbackContext) -> None:
+    user = update.message.from_user
+    update.message.reply_text(f"Hello, {user.first_name}!")
 
-logger = logging.getLogger(__name__)
-TOKEN = '6758061728:AAH52BNG1PnFKfxU3_mGsmM5Ro0j3uQXv0A'
-
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
-
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-
-def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater(TOKEN, use_context=True)
+def main() -> None:
+    # Create the Updater and pass in your bot's token
+    updater = Updater("6758061728:AAH52BNG1PnFKfxU3_mGsmM5Ro0j3uQXv0A", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
+    # Register a command handler for the /start command
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # log all errors
-    dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook(
-        listen="0.0.0.0",
-        port=int(PORT),
-        url_path=TOKEN,
-        webhook_url='https://yourherokuappname.herokuapp.com/' + TOKEN
-    )
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.start_polling()
     updater.idle()
 
 if __name__ == '__main__':
